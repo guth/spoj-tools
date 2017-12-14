145366660037332502d2c6ee4b0a2bc6c110e5db
Alex Guthrie <guthriea90@gmail.com>
2017-12-14 00:01:17 -0800

    Add a unit test.

--- mysite/spojtools/tests.py
+++ mysite/spojtools/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from . import spoj
+
+# TODO: Learn how Python does mocking and mock the remote calls.
+class SpojTests(TestCase):
+	def test_get_solved_problems_with_results(self):
+		problems = spoj.getSolvedProblems("guth")
+		self.assertTrue(len(problems) >= 116)
+		self.assertIn("CERC07K", problems)
+
+	# TODO: This now throws 404 not found. Update test accordingly.
+	# def test_get_solved_problems_with_nonexistent_user(self):
+	# 	problems = spoj.getSolvedProblems("abc123ThisUserDoesntExist456")
+	# 	self.assertEquals(problems, [])
